bcb39070020d4d148a35b82c958608e8e4184a2d
Ivan Xie <ivanxie102@gmail.com>
2025-08-29 17:27:24 -0400

    Update Home.kt

--- app/src/main/java/com/example/stretchapp/screens/Home.kt
+++ app/src/main/java/com/example/stretchapp/screens/Home.kt
@@ -3,7 +3,7 @@ package com.example.stretchapp.screens
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.Ccompose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
